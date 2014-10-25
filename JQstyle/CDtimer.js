37158617516ad25d4b8f8b58542a539995951b48
Paskal <mr.novakov@hotmail.com>
2014-10-25 17:08:29 +0300

    T1

---
+++ JQstyle/CDtimer.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+
+$('.countdown').countdown({ date: '25 October 2014 16:20:30'}, function(){
+
+
+			$('.countdown').text('DONE !');
+
+});
+
+
+
+
+setInterval(function(){
+
+
+var box_length = $('.countdown').width();
+
+var Ww = $( window ).width();
+
+$('.CD-container').css({'margin-left' : (Ww/2)-(box_length/2)+'px' });
+
+
+}, 50);
+
+
+
+
+});
