fe70de3a3a811103ec12ace746fbd1499515aec5
Paskal <mr.novakov@hotmail.com>
2014-10-31 20:26:47 +0200

    123
    123

--- JQstyle/Background.js
+++ JQstyle/Background.js
@@ -6,13 +6,45 @@ $( document ).ready(function() {
 		var Hh = $( window ).height();
 		var Ww = $( window ).width();
 
-				$(".Background").css("height", Hh+"px");
-				$(".Background").css("width", Ww+"px");
+		var Aspect_Ratio = Ww/Hh;
 
-				$(".shield-container").css({"width" : Hh*0.2+"px"});
+		var Screen_Ratio = (16/9);
 
 
-}, 50);
+
+	$(".Background , .Background-Pattern").css("height", Hh+"px");
+	$(".Background , .Background-Pattern").css("width", Ww+"px");
+	$(".Background , .Background-Pattern").css("min-height", 300+"px");
+
+
+
+
+	}, 50);
+
+
+
+
+
+//_____________________Logo_Size________________//
+
+
+
+	setInterval(function(){
+
+
+			var ScreenWidth = $( window ).width();
+			var ScreenHeight = $( window ).height();
+
+		
+		$('.shield-container').css({'width': Math.sqrt(ScreenHeight*15)+'px'});
+
+		
+			
+	
+
+	},50);
+
+
 
 
 
--- JQstyle/CDtimer.js
+++ JQstyle/CDtimer.js
@@ -1,6 +1,9 @@
 $( document ).ready(function() {
 
-$('.countdown').countdown({ date: '29 October 2014 16:20:30'}, function(){
+
+
+
+$('.countdown').countdown({ date: '29 November 2014 16:20:30'}, function(){
 
 
 			$('.countdown').text('DONE !');
@@ -21,6 +24,54 @@ $('.CD-container').css({'margin-left' : (Ww/2)-(box_length/2)+'px' });
 
 
 }, 50);
+
+
+
+//______________________________________Countdown_Timer_Screen_Size_______________________________________________//
+
+setInterval(function(){
+
+	var ScreenWidth = $( window ).width();
+	var ScreenHeight = $( window ).height();
+
+	var ScaleCoeff = 1000;
+
+	var Ratio_Width = (ScreenWidth/ScaleCoeff);
+	var Ratio_Height = (ScreenHeight/ScaleCoeff);
+
+	//alert((ScreenWidth*ScreenHeight)/1000);
+
+	
+	// $('.CDTimer').css({'margin': (((ScreenWidth/1000)*(ScreenHeight/1000))+'px'}); //
+
+	
+	
+
+			
+
+		
+
+
+
+
+
+			$('.CDTimer, .CD-number').css({'width': (ScreenWidth/13) + 'px', 'min-width' : 50 + 'px'});
+			$('.CDTimer, .CD-number').css({'height': (ScreenWidth/13) + 'px', 'min-height' : 50 + 'px'});
+
+			$('.CD-number').css({'font-size': ($('.CD-number').height()/1.8) + 'px'});
+
+
+
+
+
+
+
+	},50);
+
+
+
+	
+
 
 
 
