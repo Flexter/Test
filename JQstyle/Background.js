d7cf2b4e1b826aceee3a348991ba39f7fd72c6c5
Paskal <mr.novakov@hotmail.com>
2014-11-06 18:46:11 +0200

    111

--- JQstyle/Background.js
+++ JQstyle/Background.js
@@ -1,6 +1,13 @@
 $( document ).ready(function() {
 
+
+
+
+
 	setInterval(function(){
+
+
+
 
 
 		var Hh = $( window ).height();
@@ -26,12 +33,14 @@ var CD = position.top;
 
 
 
-	$(".Background , .Background-Pattern").css("height", Hh+"px");
-	$(".Background , .Background-Pattern").css("width", Ww+"px");
-	$(".Background , .Background-Pattern").css("min-height", LastDivHeight+CD+"px");
+	$(".Background , .Background-Pattern").css("height", LastDivHeight+CD+"px");
+	$(".Background , .Background-Pattern").css("width", "100%");
 
 
+		$('.shield-container').css({'width': Math.sqrt(Hh*15)+'px'});
 
+
+}, false);
 
 	}, 50);
 
@@ -43,20 +52,6 @@ var CD = position.top;
 
 
 
-	setInterval(function(){
-
-
-			var ScreenWidth = $( window ).width();
-			var ScreenHeight = $( window ).height();
-
-		
-		$('.shield-container').css({'width': Math.sqrt(ScreenHeight*15)+'px'});
-
-		
-			
-	
-
-	},50);
 
 
 
