a4a3cc7996e77f6afbdb868ccfe01d83714e1343
Paskal <mr.novakov@hotmail.com>
2014-11-06 20:49:39 +0200

    Progress

--- JQstyle/Background.js
+++ JQstyle/Background.js
@@ -6,56 +6,38 @@ $( document ).ready(function() {
 
 	setInterval(function(){
 
-
-
-
-
 		var Hh = $( window ).height();
 		var Ww = $( window ).width();
+		var p = $( "#last" );
+		var position = p.position();
+		var LastDivHeight = $( "#last" ).height();
+		var CD = position.top;
 
-		var Aspect_Ratio = Ww/Hh;
-
-		var Screen_Ratio = (16/9);
-
-
-
-
-
-
-
-var p = $( "#last" );
-
-var position = p.position();
-
-var LastDivHeight = $( "#last" ).height();
-
-var CD = position.top;
+		var BGHheight = $(".Background-Pattern").height();
+		var BGAdjust = $(".adjust").height();
+		var BGpostion =  ((BGHheight/2)-(BGAdjust)/2);
 
 
+		if (Hh > (LastDivHeight+CD)) {
 
-if (Hh > (LastDivHeight+CD)) {
+			$(".Background , .Background-Pattern").css("height", Hh+"px");
+			$(".Background , .Background-Pattern").css("width", "100%");
+			$(".adjust").css({"margin-top": BGpostion+"px"});
 
-	$(".Background , .Background-Pattern").css("height", Hh+"px");
-	$(".Background , .Background-Pattern").css("width", "100%");
+		}
 
-
-
-}
-
-else {
+		else {
 
 
-	$(".Background , .Background-Pattern").css("height", LastDivHeight+CD+"px");
-	$(".Background , .Background-Pattern").css("width", "100%");
+			$(".Background , .Background-Pattern").css("height", LastDivHeight+CD+"px");
+			$(".Background , .Background-Pattern").css("width", "100%");
+			$(".adjust").css({"margin-top": "0px"});
 
-};
+		};
 
+			$('.shield-container').css({'width': Math.sqrt(Hh*15)+'px'});
 
-	$('.shield-container').css({'width': Math.sqrt(Hh*15)+'px'});
-
-
-
-	}, 50);
+		}, 50);
 
 
 
